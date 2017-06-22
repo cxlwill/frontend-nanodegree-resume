209d4e57928211f8b900874ab4819dbad8aeacf8
cxlwill <cxlwill@outlook.com>
2017-06-22 13:18:54 +0800

    add personal information and change 1 style.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,92 @@
 /*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var formattedSkills = HTMLskills.replace("%data%", skills);
+var name = "Jocleyn Chen";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var role = "Web Designer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+*/
+
+//个人信息
+var bio = {
+    "name" : "Jocleyn Chen",
+    "role" : "Web Designer",
+    "contacts" : {
+        "mobile": "18750281283",
+        "email": "cxlwill@outlook.com",
+        "github": "cxlwill",
+        "Wechat": "cxlwill",
+        "location": "Xiamen"
+    },
+    "pictureURL" : "",
+    "welcomeMessage" : "Hello, World!",
+    "skills" : ["cube", "programming", "front-end"]
+};
+//工作情况
+var work = {
+    "jobs" :[
+        {
+            "emloyer": "Beijing Yingke Lawfirm",
+            "title": "Legal Assistant",
+            "location": "Xiamen",
+            "dates": "2016.6-2616.12",
+            "description": "Assisted Senior Lawyers in legal research, information disclosure and contracts drafting, involving areas of Capital Market, Real Estate and Construction."
+
+        },
+        {
+            "employer": "Guotai Junan Securities Co. Ltd.",
+            "title": "Intern",
+            "location": "Shanghai",
+            "dates": "2014.3-2014.5",
+            "description": "Assisted in bidding and distribution of government bonds and financial bonds, including drafting distribution agreements and collect published information."
+        }
+    ]
+};
+//项目背景
+var projects = {
+    "projects" :[
+        {
+            "title": "Animal Card",
+            "dates": 2017,
+            "description": "A page shows Black Face Sheep's information.",
+            "images": ""
+        },
+        {
+            "title": "Project Showpage",
+            "dates": 2017,
+            "description": "A page shows all projected finished in Udacity Front-end nanodegree.",
+            "images":""
+        }
+    ]
+};
+//教育背景
+var education = {
+    "school": [
+        {
+            "name": "Fudan University",
+            "city": "Shanghai, CN",
+            "degree": "BA",
+            "majors": ["Law"],
+            "minor": "Account",
+            "Graduation": 2014
+        },
+        {
+            "name": "Queen Mary University of London",
+            "city": "London, GB",
+            "degree": "Masters",
+            "majors": ["International Business Law"],
+            "minor": "Finance",
+            "Graduation": 2015
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title": "Front-end Nanodgree",
+            "school": "Udacity",
+            "dates": 2017,
+            "url" : ""
+        }
+    ]
+}
+
+
+
